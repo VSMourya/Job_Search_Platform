a608f0898389c7004ce63ac9f4abd57552fc93de
VSCMourya <mouryavsvp@gmail.com>
2023-11-28 01:50:51 -0500

    inital commit

---
+++ server/index.js
@@ -0,0 +1,109 @@
+const express = require("express")
+const app = express()
+
+const mysql2 = require("mysql2")
+const cors = require("cors")
+
+app.use(cors())
+
+const db = mysql2.createConnection({
+    user:"root",
+    host:"localhost",
+    password: "12345678",
+    database:"JobSearch"
+})
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+// Middleware to parse JSON and URL-encoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/create', (req, res) => {
+    const { type, data } = req.body; // 'type' can be 'employer', 'candidate', or 'job'
+
+    console.log("data==> :",data)
+    if (type === 'Employers') {
+        // const { employerId, EnterpriseName, Location, Address } = data;
+
+        const EmployerId = data.employerId
+        const EnterpriseName = data.enterpriseName
+        const Location = data.location
+        const Address = data.address
+        const CEO = data.CEO
+
+        const sqlInsert = "INSERT INTO employers (EmployerID, EnterpriseName, CEO, Location, Address) VALUES (?,?,?,?,?)";
+        db.query(sqlInsert, [ EmployerId, EnterpriseName,CEO, Location, Address], (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Error while creating employee'+err.message);
+            } else {
+                res.status(201).send('Employee created successfully');
+            }
+        });
+
+    } else if (type === 'Candidates') {
+        // Assuming data contains { candidateId, qualification, experience }
+        const { candidateId, qualification, experience } = data;
+        const sqlInsert = "INSERT INTO candidates (candidateID, qualification, experience) VALUES (?, ?, ?)";
+        db.query(sqlInsert, [candidateId, qualification, experience], (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Error while creating candidate');
+            } else {
+                res.status(201).send('Candidate created successfully');
+            }
+        });
+    } else if (type === 'Jobs') {
+        // Assuming data contains { jobId, jobDescription, salary, employerId }
+        const { jobId, jobDescription, salary, employerId } = data;
+        const sqlInsert = "INSERT INTO jobs (JobID, JobDescription, Salary, EmployerID) VALUES (?, ?, ?, ?)";
+        db.query(sqlInsert, [jobId, jobDescription, salary, employerId], (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Error while creating job');
+            } else {
+                res.status(201).send('Job created successfully');
+            }
+        });
+    } else {
+        res.status(400).send('Invalid type specified');
+    }
+});
+
+app.get('/employers', (req, res) => {
+    db.query("SELECT * FROM employers", (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
+app.get('/jobs', (req, res) => {
+    db.query("SELECT * FROM jobs", (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  })
+  
+app.get('/candidates', (req, res) => {
+      db.query("SELECT * FROM candidates", (err, result) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(result);
+        }
+      });
+    });
+  ;
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
